b85e256b105762bab8b0ae3e3f7d3a8129238fc2
peter3374 <peter4533@mail.ru>
2023-01-23 00:09:18 +0300

    format added

--- lib/screens/task/widgets/task_list.dart
+++ lib/screens/task/widgets/task_list.dart
@@ -41,100 +41,106 @@ class TaskList extends StatelessWidget {
                     TaskModel? task = box.getAt(i);
                     DateTime? deadline = task!.deadlineDateTime;
 
-                    return Slidable(
-                      startActionPane: ActionPane(
-                        motion: const ScrollMotion(),
-                        children: [
-                          SlidableAction(
-                            flex: 2,
-                            onPressed: (_) => CategoryFinder()
-                                .checkCategoryIsNotEmpty(context, i),
-                            backgroundColor: Colors.orange,
-                            foregroundColor: Colors.white,
-                            icon: Icons.edit,
-                            label: 'Edit',
-                          ),
-                          SlidableAction(
-                            flex: 2,
-                            onPressed: (task.isDone)
-                                ? null
-                                : (_) async {
-                                    //TODO  put in controller
-                                    await box.putAt(
-                                      i,
-                                      TaskModel(
-                                        id: CategoryIndexProvider()
-                                            .getCategoryIndex(task.category),
-                                        creationDate: task.creationDate,
-                                        deadlineDateTime: task.deadlineDateTime,
-                                        text: task.text,
+                    if (DateFormat.yMd().format(deadline) ==
+                        DateFormat.yMd().format(selectedDate)) {
+                      return Slidable(
+                        startActionPane: ActionPane(
+                          motion: const ScrollMotion(),
+                          children: [
+                            SlidableAction(
+                              flex: 2,
+                              onPressed: (_) => CategoryFinder()
+                                  .checkCategoryIsNotEmpty(context, i),
+                              backgroundColor: Colors.orange,
+                              foregroundColor: Colors.white,
+                              icon: Icons.edit,
+                              label: 'Edit',
+                            ),
+                            SlidableAction(
+                              flex: 2,
+                              onPressed: (task.isDone)
+                                  ? null
+                                  : (_) async {
+                                      //TODO  put in controller
+                                      await box.putAt(
+                                        i,
+                                        TaskModel(
+                                          id: CategoryIndexProvider()
+                                              .getCategoryIndex(task.category),
+                                          creationDate: task.creationDate,
+                                          deadlineDateTime:
+                                              task.deadlineDateTime,
+                                          text: task.text,
+                                          category: task.category,
+                                          isDone: true,
+                                        ),
+                                      );
+                                    },
+                              backgroundColor: Colors.green,
+                              foregroundColor: Colors.white,
+                              icon: Icons.done,
+                              label: 'Done',
+                            ),
+                          ],
+                        ),
+                        endActionPane: ActionPane(
+                          motion: const ScrollMotion(),
+                          children: [
+                            SlidableAction(
+                              flex: 2,
+                              onPressed: (_) async {
+                                // remove
+                                await ArchieveRepositoryImpl().database.add(
+                                      ArchieveModel(
                                         category: task.category,
-                                        isDone: true,
+                                        text: task.text,
+                                        deadlineDateTime: task.deadlineDateTime,
                                       ),
                                     );
-                                  },
-                            backgroundColor: Colors.green,
-                            foregroundColor: Colors.white,
-                            icon: Icons.done,
-                            label: 'Done',
-                          ),
-                        ],
-                      ),
-                      endActionPane: ActionPane(
-                        motion: const ScrollMotion(),
-                        children: [
-                          SlidableAction(
-                            flex: 2,
-                            onPressed: (_) async {
-                              // remove
-                              await ArchieveRepositoryImpl().database.add(
-                                    ArchieveModel(
-                                      category: task.category,
-                                      text: task.text,
-                                      deadlineDateTime: task.deadlineDateTime,
-                                    ),
-                                  );
-                              await box.deleteAt(i);
-                            },
-                            backgroundColor: Colors.red,
-                            foregroundColor: Colors.white,
-                            icon: Icons.archive,
-                            label: 'Remove',
-                          ),
-                        ],
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 10),
-                        child: TimelineTile(
-                          node: TimelineNode(
-                            indicator: (task.isDone)
-                                ? const IconDoneWidget()
-                                : const IconNotDoneWidget(),
-                            startConnector: (task.isDone)
-                                ? const ConnectorDoneWidget()
-                                : const ConnectorNotDoneWidget(),
-                            endConnector: (task.isDone)
-                                ? const ConnectorDoneWidget()
-                                : const ConnectorNotDoneWidget(),
-                          ),
-                          oppositeContents: null,
-                          nodeAlign: TimelineNodeAlign.start,
-                          contents: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Text(
-                                DateFormat('hh:mm a').format(deadline),
-                                style: const TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w400,
+                                await box.deleteAt(i);
+                              },
+                              backgroundColor: Colors.red,
+                              foregroundColor: Colors.white,
+                              icon: Icons.archive,
+                              label: 'Remove',
+                            ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: TimelineTile(
+                            node: TimelineNode(
+                              indicator: (task.isDone)
+                                  ? const IconDoneWidget()
+                                  : const IconNotDoneWidget(),
+                              startConnector: (task.isDone)
+                                  ? const ConnectorDoneWidget()
+                                  : const ConnectorNotDoneWidget(),
+                              endConnector: (task.isDone)
+                                  ? const ConnectorDoneWidget()
+                                  : const ConnectorNotDoneWidget(),
+                            ),
+                            oppositeContents: null,
+                            nodeAlign: TimelineNodeAlign.start,
+                            contents: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text(
+                                  DateFormat('hh:mm a').format(deadline),
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w400,
+                                  ),
                                 ),
-                              ),
-                              TaskCardWidget(taskModel: task),
-                            ],
+                                TaskCardWidget(taskModel: task),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    );
+                      );
+                    } else {
+                      return const SizedBox();
+                    }
                   });
         });
   }
