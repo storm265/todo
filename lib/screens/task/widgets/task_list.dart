febf76ab048365086ba4035327af3d77b5dd585a
peter3374 <peter4533@mail.ru>
2023-01-23 00:01:00 +0300

    dateformat

--- lib/screens/task/widgets/task_list.dart
+++ lib/screens/task/widgets/task_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:intl/intl.dart';
 import 'package:timelines/timelines.dart';
 import 'package:todo/service/common/category_finder.dart';
 import 'package:todo/service/common/category_index_provider.dart';
@@ -40,106 +41,100 @@ class TaskList extends StatelessWidget {
                     TaskModel? task = box.getAt(i);
                     DateTime? deadline = task!.deadlineDateTime;
 
-                    if (deadline.toString().substring(0, 11) ==
-                        selectedDate.toString().substring(0, 11)) {
-                      return Slidable(
-                        startActionPane: ActionPane(
-                          motion: const ScrollMotion(),
-                          children: [
-                            SlidableAction(
-                              flex: 2,
-                              onPressed: (_) => CategoryFinder()
-                                  .checkCategoryIsNotEmpty(context, i),
-                              backgroundColor: Colors.orange,
-                              foregroundColor: Colors.white,
-                              icon: Icons.edit,
-                              label: 'Edit',
-                            ),
-                            SlidableAction(
-                              flex: 2,
-                              onPressed: (task.isDone)
-                                  ? null
-                                  : (_) async {
-                                      //TODO  put in controller
-                                      await box.putAt(
-                                        i,
-                                        TaskModel(
-                                          id: CategoryIndexProvider()
-                                              .getCategoryIndex(task.category),
-                                          creationDate: task.creationDate,
-                                          deadlineDateTime:
-                                              task.deadlineDateTime,
-                                          text: task.text,
-                                          category: task.category,
-                                          isDone: true,
-                                        ),
-                                      );
-                                    },
-                              backgroundColor: Colors.green,
-                              foregroundColor: Colors.white,
-                              icon: Icons.done,
-                              label: 'Done',
-                            ),
-                          ],
-                        ),
-                        endActionPane: ActionPane(
-                          motion: const ScrollMotion(),
-                          children: [
-                            SlidableAction(
-                              flex: 2,
-                              onPressed: (_) async {
-                                // remove
-                                ArchieveRepositoryImpl().database.add(
-                                      ArchieveModel(
-                                        category: task.category,
-                                        text: task.text,
+                    return Slidable(
+                      startActionPane: ActionPane(
+                        motion: const ScrollMotion(),
+                        children: [
+                          SlidableAction(
+                            flex: 2,
+                            onPressed: (_) => CategoryFinder()
+                                .checkCategoryIsNotEmpty(context, i),
+                            backgroundColor: Colors.orange,
+                            foregroundColor: Colors.white,
+                            icon: Icons.edit,
+                            label: 'Edit',
+                          ),
+                          SlidableAction(
+                            flex: 2,
+                            onPressed: (task.isDone)
+                                ? null
+                                : (_) async {
+                                    //TODO  put in controller
+                                    await box.putAt(
+                                      i,
+                                      TaskModel(
+                                        id: CategoryIndexProvider()
+                                            .getCategoryIndex(task.category),
+                                        creationDate: task.creationDate,
                                         deadlineDateTime: task.deadlineDateTime,
+                                        text: task.text,
+                                        category: task.category,
+                                        isDone: true,
                                       ),
                                     );
-                                await box.deleteAt(i);
-                              },
-                              backgroundColor: Colors.red,
-                              foregroundColor: Colors.white,
-                              icon: Icons.archive,
-                              label: 'Remove',
-                            ),
-                          ],
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 30),
-                          child: TimelineTile(
-                            node: TimelineNode(
-                              indicator: (task.isDone)
-                                  ? const IconDoneWidget()
-                                  : const IconNotDoneWidget(),
-                              startConnector: (task.isDone)
-                                  ? const ConnectorDoneWidget()
-                                  : const ConnectorNotDoneWidget(),
-                              endConnector: (task.isDone)
-                                  ? const ConnectorDoneWidget()
-                                  : const ConnectorNotDoneWidget(),
-                            ),
-                            oppositeContents: null,
-                            nodeAlign: TimelineNodeAlign.start,
-                            contents: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Text(
-                                  deadline.toIso8601String().substring(11, 16),
-                                  style: const TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w400,
-                                  ),
+                                  },
+                            backgroundColor: Colors.green,
+                            foregroundColor: Colors.white,
+                            icon: Icons.done,
+                            label: 'Done',
+                          ),
+                        ],
+                      ),
+                      endActionPane: ActionPane(
+                        motion: const ScrollMotion(),
+                        children: [
+                          SlidableAction(
+                            flex: 2,
+                            onPressed: (_) async {
+                              // remove
+                              await ArchieveRepositoryImpl().database.add(
+                                    ArchieveModel(
+                                      category: task.category,
+                                      text: task.text,
+                                      deadlineDateTime: task.deadlineDateTime,
+                                    ),
+                                  );
+                              await box.deleteAt(i);
+                            },
+                            backgroundColor: Colors.red,
+                            foregroundColor: Colors.white,
+                            icon: Icons.archive,
+                            label: 'Remove',
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: TimelineTile(
+                          node: TimelineNode(
+                            indicator: (task.isDone)
+                                ? const IconDoneWidget()
+                                : const IconNotDoneWidget(),
+                            startConnector: (task.isDone)
+                                ? const ConnectorDoneWidget()
+                                : const ConnectorNotDoneWidget(),
+                            endConnector: (task.isDone)
+                                ? const ConnectorDoneWidget()
+                                : const ConnectorNotDoneWidget(),
+                          ),
+                          oppositeContents: null,
+                          nodeAlign: TimelineNodeAlign.start,
+                          contents: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                DateFormat('hh:mm a').format(deadline),
+                                style: const TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w400,
                                 ),
-                                TaskCardWidget(taskModel: task),
-                              ],
-                            ),
+                              ),
+                              TaskCardWidget(taskModel: task),
+                            ],
                           ),
                         ),
-                      );
-                    } else {
-                      return const SizedBox();
-                    }
+                      ),
+                    );
                   });
         });
   }
